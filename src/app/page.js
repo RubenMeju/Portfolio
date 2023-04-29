6710b3b1b4480d5a3f14bc1af37b7a38197eb762
RubenMeju <rubenmeju@outlook.es>
2023-04-29 15:45:17 +0200

    home

--- src/app/page.js
+++ src/app/page.js
@@ -3,39 +3,39 @@ import Link from 'next/link'
 import styles from './page.module.css'
 import Image from 'next/image'
 
-import imgProfile from '../../public/SonGoku.webp'
+import imgYo from '../../public/SonGoku.webp'
 
 export default function Home() {
   return (
     <div className={styles.home}>
-      <h1>Meju</h1>
-      {/*
+      <div className={styles.header}>
+        <div className={styles.contImg}>
+          <Image src={imgYo} alt='Rubén Yáñez' fill />
+        </div>
+      </div>
 
-      <div className={styles.container}>
-        <div className={styles.description}>
-          <h1>
+      <div className={styles.body}>
+        <div>
+          <h1 className={styles.textIntro}>
             Hey there, my name is Rubén Yáñez. I am a Fullstack Web Developer
             based in La Coruña, Spain.
           </h1>
-          <p>
+          <h3 className={styles.textDescription}>
             I am a self-taught developer with a passion for learning new things
-            and solving problems. I am currently working as a freelance
-            developer and I am open to new opportunities.
-          </p>
-
-          <Link href='/projects'>projects </Link>
+            and solving problems. I am currently working as a freelancer, but I
+            am looking for new opportunities to grow as a developer.
+          </h3>
         </div>
-        <div className={styles.contImage}>
-          <Image
-            width={200}
-            height={200}
-            src={imgProfile}
-            className={styles.img}
-            alt='Rubén'
-          />
+
+        <div className={styles.contLinks}>
+          <Link href='projects' className={styles.link}>
+            Projects
+          </Link>
+          <Link href='contact' className={styles.link}>
+            Contact
+          </Link>
         </div>
       </div>
-  */}
     </div>
   )
 }
