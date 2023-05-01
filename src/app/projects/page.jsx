f9320922179d218f190dd3372774b917e4d2a7aa
RubenMeju <rubenmeju@outlook.es>
2023-05-01 23:21:44 +0200

    refactor listProjects

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,47 +1,7 @@
 import styles from './projects.module.css'
-import imagen from '../../../public/SonGoku.webp'
 import Card from '../components/card/Card'
 
-import imgTetris from '../../../public/tetris.png'
-
-const projects = [
-  {
-    title: 'Tetris',
-    description: 'El juego de tetris',
-    image: imgTetris,
-    link: 'https://github.com/RubenMeju/TetrisReduxToolkit/blob/main/tetris.png?raw=true'
-  },
-  {
-    title: 'Project 2',
-    description: 'This is a project',
-    image: imagen,
-    link: 'https://www.google.com'
-  },
-  {
-    title: 'Project 3',
-    description: 'This is a project',
-    image: imagen,
-    link: 'https://www.google.com'
-  },
-  {
-    title: 'Project 1',
-    description: 'This is a project',
-    image: imagen,
-    link: 'https://www.google.com'
-  },
-  {
-    title: 'Project 2',
-    description: 'This is a project',
-    image: imagen,
-    link: 'https://www.google.com'
-  },
-  {
-    title: 'Project 3',
-    description: 'This is a project',
-    image: imagen,
-    link: 'https://www.google.com'
-  }
-]
+import { projects } from './projectList'
 
 export default function Portfolio() {
   return (
---
+++ src/app/projects/projectList.js
@@ -0,0 +1,41 @@
+import imgTetris from '../../../public/tetris.jpg'
+import imagen from '../../../public/SonGoku.webp'
+
+export const projects = [
+  {
+    title: 'Tetris',
+    description: 'El juego de tetris',
+    image: imgTetris,
+    link: 'https://github.com/RubenMeju/TetrisReduxToolkit/blob/main/tetris.png?raw=true'
+  },
+  {
+    title: 'Project 2',
+    description: 'This is a project',
+    image: imagen,
+    link: 'https://www.google.com'
+  },
+  {
+    title: 'Project 3',
+    description: 'This is a project',
+    image: imagen,
+    link: 'https://www.google.com'
+  },
+  {
+    title: 'Project 1',
+    description: 'This is a project',
+    image: imagen,
+    link: 'https://www.google.com'
+  },
+  {
+    title: 'Project 2',
+    description: 'This is a project',
+    image: imagen,
+    link: 'https://www.google.com'
+  },
+  {
+    title: 'Project 3',
+    description: 'This is a project',
+    image: imagen,
+    link: 'https://www.google.com'
+  }
+]
