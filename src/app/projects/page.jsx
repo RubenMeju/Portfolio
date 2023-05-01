9c4ca80eb7f8a768de12922d89330cee4098a22d
RubenMeju <rubenmeju@outlook.es>
2023-05-01 23:08:53 +0200

    styles, responsive

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -2,12 +2,14 @@ import styles from './projects.module.css'
 import imagen from '../../../public/SonGoku.webp'
 import Card from '../components/card/Card'
 
+import imgTetris from '../../../public/tetris.png'
+
 const projects = [
   {
-    title: 'Project 1',
-    description: 'This is a project',
-    image: imagen,
-    link: 'https://www.google.com'
+    title: 'Tetris',
+    description: 'El juego de tetris',
+    image: imgTetris,
+    link: 'https://github.com/RubenMeju/TetrisReduxToolkit/blob/main/tetris.png?raw=true'
   },
   {
     title: 'Project 2',
