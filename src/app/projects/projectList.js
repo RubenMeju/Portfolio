fee6f6771c3d643655f4da02303a3ba53a2b1ad5
RubenMeju <rubenmeju@outlook.es>
2023-05-08 18:01:12 +0200

    clean list projects

--- src/app/projects/projectList.js
+++ src/app/projects/projectList.js
@@ -37,41 +37,5 @@ export const projects = [
     technology: ['React', 'Next', 'HTML', 'CSS', 'Framer motion', 'Vercel'],
     url_vercel: 'https://portfolio-rubenmeju.vercel.app/',
     url_github: 'https://github.com/RubenMeju/Portfolio'
-  },
-  {
-    title: 'Project 4',
-    description: 'This is a project',
-    image: imagen,
-    link: 'https://www.google.com'
-  },
-  {
-    title: 'Project 5',
-    description: 'This is a project',
-    image: imagen,
-    link: 'https://www.google.com'
-  },
-  {
-    title: 'Project 6',
-    description: 'This is a project',
-    image: imagen,
-    link: 'https://www.google.com'
-  },
-  {
-    title: 'Tetris',
-    description: 'El juego de tetris',
-    image: imgTetris,
-    link: 'https://tetris-redux-toolkit.vercel.app/'
-  },
-  {
-    title: 'Movies API',
-    description: 'API de peliculas',
-    image: imgMovies,
-    link: 'https://movies-api-tmdb.vercel.app/'
-  },
-  {
-    title: 'Project 9',
-    description: 'This is a project',
-    image: imagen,
-    link: 'https://www.google.com'
   }
 ]
