90572faab4e4c9e9353a34c8c23daddbd7bbde4e
RubenMeju <rubenmeju@outlook.es>
2023-05-08 15:53:35 +0200

    my photo

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import styles from './page.module.css'
 import Image from 'next/image'
 
-import imgYo from '../../public/SonGoku.webp'
+import imgYo from '../../public/yo1.webp'
 
 export default function Home() {
   return (
