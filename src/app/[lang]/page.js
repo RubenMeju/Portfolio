98385b56ec19ae6b4d7734c4ff0fc2e207f10ceb
RubenMeju <rubenmeju@outlook.es>
2023-05-14 21:30:12 +0200

    change image

--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import styles from './page.module.css'
 import Image from 'next/image'
 import { getDictionary } from '../../get-dictionary'
-import imgYo from '../../../public/yo1.jpg'
+import imgYo from '../../../public/yo.jpg'
 
 export default async function Home({ params: { lang } }) {
   const dictionary = await getDictionary(lang)
